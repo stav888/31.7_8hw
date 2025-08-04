7efa3d0c27b0b13484562957b46a9e770cb82a01
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-04 13:31:05 +0300

    Create Exercise 4.sql

---
+++ Exercise 4.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION div_mod(a DOUBLE PRECISION, b DOUBLE PRECISION, OUT quotient DOUBLE PRECISION, OUT remainder DOUBLE PRECISION)
+AS $$
+BEGIN
+    quotient := a / b;
+    remainder := a % b;
+END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT * FROM div_mod(17, 5);

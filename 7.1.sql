9617d5d99380565f0b5838d8f535e0bca3289c35
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-05 16:38:14 +0300

    Create 7.1.sql

---
+++ 7.1.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION sp_latest_book(
+    OUT title TEXT,
+    OUT publish_date DATE
+)
+RETURNS RECORD
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT b.title, b.publish_date
+    FROM books b
+    ORDER BY b.publish_date DESC
+    LIMIT 1;
+END;
+$$;
+
+
+SELECT * FROM sp_latest_book();

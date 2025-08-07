d5eaeb60ecafaa996c550cc99b1131266282dfde
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-07 14:12:17 +0300

    Update Exercise 7.sql

--- Exercise 7.sql
+++ Exercise 7.sql
@@ -1,19 +1,16 @@
 DROP FUNCTION IF EXISTS sp_latest_book();
 
-CREATE OR REPLACE FUNCTION sp_latest_book(
-    OUT title TEXT,
-    OUT publish_date DATE
+CREATE OR REPLACE FUNCTION sp_latest_book()
+RETURNS TABLE (
+    title TEXT,
+    publish_date DATE
 )
-RETURNS RECORD
-LANGUAGE plpgsql
+LANGUAGE sql
 AS $$
-BEGIN
-    RETURN QUERY
     SELECT b.title, b.publish_date
     FROM books b
     ORDER BY b.publish_date DESC
     LIMIT 1;
-END;
 $$;
 
 

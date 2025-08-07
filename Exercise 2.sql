cc610e51959a5e2e896cc3aea00104fc831676eb
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-07 14:13:21 +0300

    Update Exercise 2.sql

--- Exercise 2.sql
+++ Exercise 2.sql
@@ -1,8 +1,8 @@
 DROP FUNCTION IF EXISTS create_orders_table();
 
 CREATE OR REPLACE PROCEDURE create_orders_table()
-LANGUAGE plpgsql
-AS $$
+LANGUAGE plpgsql AS
+$$
 BEGIN
     CREATE TABLE IF NOT EXISTS orders (
         id SERIAL PRIMARY KEY,

b6507310a993983ea9e430361317f14493df4c4c
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-07 10:10:20 +0300

    Update Exercise 1.sql

--- Exercise 1.sql
+++ Exercise 1.sql
@@ -1,11 +1,12 @@
 DROP FUNCTION IF EXISTS greet_user(TEXT);
 
 CREATE OR REPLACE FUNCTION greet_user(name TEXT)
-RETURNS TEXT AS $$
+RETURNS TEXT 
+LANGUAGE plpgsql AS
+$$
 BEGIN
     RETURN 'Hello ' || name || '! The time is ' || CURRENT_TIMESTAMP;
 END;
-$$ LANGUAGE plpgsql;
-
+$$;
 
 SELECT greet_user('Alex');
